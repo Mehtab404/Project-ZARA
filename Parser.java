951d38f25f8a2bf34498011872741d9a2b427be9
Rajiv Kumar <rajiv.ranjan17092005@gmail.com>
2026-04-05 22:01:16 +0530

    Add statement parsing (SET, SHOW, WHEN, LOOP)

--- Parser.java
+++ Parser.java
@@ -12,10 +12,28 @@ public class Parser {
         List<Instruction> instructions = new ArrayList<>();
 
         while (!isAtEnd()) {
-            if (match(TokenType.NEWLINE)) continue;
+            if (match(TokenType.NEWLINE))
+                continue;
             instructions.add(parseStatement());
         }
 
         return instructions;
+    }
+
+    private Instruction parseStatement() {
+
+        if (match(TokenType.SET))
+            return parseAssign();
+
+        if (match(TokenType.SHOW))
+            return parsePrint();
+
+        if (match(TokenType.WHEN))
+            return parseIf();
+
+        if (match(TokenType.LOOP))
+            return parseLoop();
+
+        throw new RuntimeException("Unexpected token: " + peek().getValue());
     }
 }
