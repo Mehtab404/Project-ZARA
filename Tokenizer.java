467db84ed043bee64c6026c935b09bd70a0b4ff4
RITESH JHA <riteshjha2244dtu@gmail.com>
2026-04-07 12:22:20 +0530

    import Library

--- Tokenizer.java
+++ Tokenizer.java
@@ -1,3 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
 public class Tokenizer {
+    private final String source;
+    private int pos;
+    private int line = 1;
+
+    public Tokenizer(String source) {
+        this.source = source;
+    }
     
 }
