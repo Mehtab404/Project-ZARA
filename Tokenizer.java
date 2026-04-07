8f2c02b925db04f211553b91dfa6c5805470ca68
RITESH JHA <riteshjha2244dtu@gmail.com>
2026-04-07 17:23:21 +0530

    added string, number and newline handling in tokenizer

--- Tokenizer.java
+++ Tokenizer.java
@@ -8,5 +8,27 @@ public class Tokenizer {
     public Tokenizer(String source) {
         this.source = source;
     }
+
+    public List<Token> tokenize() {
+        List<Token> tokenList = new ArrayList<>();
+        while (pos < source.length()) {
+            char ch = source.charAt(pos);
+            if (ch == '\n') {
+                tokenList.add(new Token(TokenType.NEWLINE, "\\n", line));
+                line++;
+                pos++;
+                continue;
+            }
+            if (ch == '"') {
+                tokenList.add(readString());
+                continue;
+            }
+
+            if (Character.isDigit(ch)) {
+                tokenList.add(readNumber());
+                continue;
+            }
+        }
     
+    }
 }
